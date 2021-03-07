0e34ee0a8a9f55ea5b4ae2a24279d8c67aa8754f
danielferreira3 <daniel.z.ferreira@hotmail.com>
2021-03-06 21:57:20 -0300

    pagina cadastrar ok

---
+++ Front end/testeMv/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CadastrarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
